d7444f01e40655077b6ec7b89a30c035dd0b8d34
ViniBiss02 <viniciusbiss02@gmail.com>
2025-04-25 18:42:57 -0300

    Criando template para exibir  lista de clintes cadastrado.

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from .views import ClienteCreateView, ClienteListView
 
 urlpatterns = [
-
+    path('form_clientes/', ClienteCreateView.as_view()),
+    # Add other URL patterns here as needed
+    path('lista_clientes/', ClienteListView.as_view(), name='lista_clientes'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
+from .models import Cliente
 
-# Create your views here.
+class ClienteCreateView(CreateView):
+    model = Cliente
+    fields = "__all__"
+    template_name = "form_cliente.html"
+    success_url = reverse_lazy("lista_clientes")  # Certifique-se que na urls.py a rota tem o nome "lista_clientes"
+
+class ClienteListView(ListView):
+    model = Cliente
+    template_name = "Lista_clientes.html"
