6d9f8ebe5a7d8b099ced34d1903ce2209aa49198
ViniBiss02 <viniciusbiss02@gmail.com>
2025-04-25 10:48:29 -0300

    criando tabela no banco de dados.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    data_nasciemento = models.DateField(null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    profissao = models.CharField(max_length=100, null=False, blank=False)
